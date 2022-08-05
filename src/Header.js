651abd7b4340e72932eedb5ccfd4ad7ab554bf2a
Yohannes Hailu <yohanneshdev21@gmail.com>
2022-08-05 14:17:51 +0300

    Header Component started

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { IconButton } from "@mui/material";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+          alt=""
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search Mail" />
+        <ArrowDownwardIcon className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
